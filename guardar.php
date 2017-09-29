aed2904a515f3d9b92462443434a457ba8dd7819
chriscastro182 <deck.chris.182@gmail.com>
2017-09-29 03:51:02 -0500

    Hace INSERT y calcula

--- guardar.php
+++ guardar.php
@@ -1,41 +1,75 @@
 <?php
-require 'conexion.php';
+require("include/conexion.php");
 
-$f_ingreso = $_POST['f_ingreso'];
+
+$ingreso="";
+$salida="";
+
+$ingreso = isset($_POST['ingreso']) ? $_POST['ingreso'] : '';
 $guiaMaster = $_POST['guiaMaster'];
 $guiaHouse = $_POST['guiaHouse'];
 $piezas = $_POST['piezas'];
-$peso = $_POST['peso'];
+$peso = isset($_POST['peso']) ? $_POST['peso'] : '';
 $descripcion = $_POST['descripcion'];
 $oficioAduana = $_POST['oficioAduana'];
-$f_salida = $_POST['f_salida'];
-$diasTotales = $_POST['diasTotales'];
+$salida = isset($_POST['salida']) ? $_POST['salida'] : '';
+$diasTotales = isset($_POST['diasTotales']) ? $_POST['diasTotales'] : '';
 $estatus = $_POST['estatus'];
-$derechos = $_POST['derechos']
-$excepcion = $_POST['excepcion'];
+$derechos = 0;
+$excepcion = isset($_POST['excepcion']) ? $_POST['excepcion'] : '';
 
-$sql = "INSERT INTO `registroabandono` (`f_ingreso`, `guiaMaster`, `guiaHouse`, `piezas`, `peso`, `descripcion`, `oficioAduana`, `f_salida`, `diasTotales`, `estatus`, `derechos`, `excepcion`)
-                            VALUES ('$f_ingreso', '$guiaMaster', '$guiaHouse', '$piezas', '$peso', '$descripcion', '$oficioAduana', '$f_salida', '$diasTotales', '$estatus', '$derechos', '$excepcion');"
+function diffDias(){
+  $fUno=strtotime($ingreso=isset($_POST['ingreso']) ? $_POST['ingreso'] : '');
+  $fDos=strtotime($salida=isset($_POST['salida']) ? $_POST['salida'] : '');
+  return ceil(abs($fDos - $fUno) / 86400);
+}
+  $diasTotales=diffDias(); //función vacía que calcula la diferencia en días entre
+  $pesoC=0;              // Fecha de ingreso y Fecha de salida, asignándola a $diasTotales
+  if($excepcion=="Efectos Personales"){
+    $pesoC=$peso/100;
+    $tarifaEf=18.60*$pesoC;
+    $derechos=$tarifaEf*$diasTotales;
+  }else {
+    $pesoC=$peso/500;
+    // Función vacía que le da valor a la variable $tarifa
+    $tarifa=calcTarifa();
+    $tarifa=$pesoC*$tarifa;
+    if($excepcion=="Especial"){
+      $tarifaEs=$tarifa*2; // $tarifaEs representa al doble aplicado en mercancía Especial
+                          //NO CONFUNDIR con $tarifaef que aplica para mercancía de Efectos Personales.
+      $derechos=$tarifaEs*$diasTotales;
+    } else{
+        $derechos=$tarifa*$diasTotales;
+    }
+  }
+function calcTarifa(){
+  $diasTotales=diffDias();
+  $tarifa=0;
+  if ($diasTotales<=15) {
+    return 11.46;
+  }
+  elseif ($diasTotales>15 && $diasTotales <=45) {
+    return 22.34;
+  }
+  elseif ($diasTotales>45) {
+    return 36.20;
+  }
+}
+$sql = "INSERT INTO registroabandono (f_ingreso, guiaMaster, guiaHouse, piezas, peso, descripcion, oficioAduana, f_salida, diasTotales, estatus, derechos, excepcion) VALUES ('$ingreso','$guiaMaster','$guiaHouse','$piezas','$peso','$descripcion','$oficioAduana','$salida','$diasTotales','$estatus','$derechos','$excepcion')";
+
 $resultado = $mysqli->query($sql);
-
+echo $sql;
 ?>
 
 <html lang="es">
-<head>
-
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/bootstrap-theme.css" rel="stylesheet">
-  <script src="js/jquery-3.1.1.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-</head>
-
+<?php require("head.php") ?>
 <body>
   <div class="container">
     <div class="row">
       <div class="row" style="text-align:center">
         <?php if($resultado) { ?>
-          <h3>REGISTRO GUARDADO</h3>
+          header( 'Location: index.php' );
+
           <?php } else { ?>
           <h3>ERROR AL GUARDAR</h3>
         <?php } ?>
--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link href="css/bootstrap.min.css" rel="stylesheet">
-     <link rel="shortcut icon" href="favicon.ico">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
-
-    <title>Cálculo de Abandono</title>
-  </head>
+  <?php require("head.php") ?>
   <body>
     <?php
       require("include/conexion.php");
@@ -34,8 +24,7 @@
         </div>
       </div>
       <div class="row">
-
-          <form >
+          <form  class="form-horizontal" method="POST" action="guardar.php">
               <!-- Aquí van los campos -->
               <div class="container">
                 <table class="table table-bordered table-condensed">
@@ -49,29 +38,25 @@
                       <th>Descripcion</th>
                       <th>Oficio de Aduana</th>
                       <th>Fecha de Salida</th>
-                      <th>Días totales</th>
                       <th>Estatus</th>
-                      <th>Derechos</th>
                       <th>Excepción</th>
                     </tr>
                   </thead>
                   <tbody>
                       <tr>
-                         <td><input type="date" id="f_ingreso" name="f_ingreso" class="form-control" required/></td>
+                         <td><input type="date" id="ingreso" name="ingreso" class="form-control" required/></td>
                          <td><input type="text" id="guiaMaster" name="guiaMaster" class="form-control" required/></td>
                          <td><input type="text" id="guiaHouse" name="guiaHouse" class="form-control" required/></td>
                          <td><input type="number" id="piezas" name="piezas"class="form-control" required/></td>
                          <td><input type="number" name="peso" class="form-control" id="peso" min="1" max="100000" required></td>
                          <td><input type="text" id="descripcion" name="descripcion" class="form-control"/></td>
                          <td><input type="text" id="oficioAduana" name="oficioAduana" class="form-control"/></td>
-                         <td><input type="date" id="f_salida" name="salida"  class="form-control" required/></td>
-                         <td><input type="number" id="diasTotales" name="diasTotales" class="form-control" /></td>
+                         <td><input type="date" id="salida" name="salida"  class="form-control" required/></td>
                          <td><select class="form-control" id="estatus" name="estatus">
-                              <option value="Inactivo"></option>
-                              <option value="Activo"></option>
+                              <option value="Inactivo">Inactivo</option>
+                              <option value="Activo">Activo</option>
                 						</select>
-                        <td><input type="number" id="derechos" name="derechos" class="form-control" /></td>
-                         <td><select class="form-control" id="excepcion" name="excepcion" >
+                        <td><select class="form-control" id="excepcion" name="excepcion" >
                                <option value="Normal">Normal</option>
                                <option value="Especial">Especial</option>
                                <option value="Efectos Personales">Efectos Personales</option>
@@ -80,15 +65,11 @@
                       </tr>
                   </tbody>
                 </table>
-                <button type="button">Calcular días</button>
-                <button type="submit" name="button">Calcular tarifa</button>
+                <button class="btn btn-primary" type="submit">Calcular días</button>
               </div>
           </form>
       </div>
-      <?php if (isset($_POST['button']))
-      {
-        calcular($id);
-      } ?>
+
     <div class="container">
       <h4>Mercancías Especiales:</h4>
       <ul>
@@ -101,20 +82,6 @@
         <li>e) Los animales vivos.</li>
       </ul>
     </div>
-  <script>
-  $(function() {
-    // for bootstrap 3 use 'shown.bs.tab', for bootstrap 2 use 'shown' in the next line
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        // save the latest tab; use cookies if you like 'em better:
-        localStorage.setItem('lastTab', $(this).attr('href'));
-    });
 
-    // go to the latest tab, if it exists:
-    var lastTab = localStorage.getItem('lastTab');
-    if (lastTab) {
-        $('[href="' + lastTab + '"]').tab('show');
-    }
-  });
-  </script>
   </body>
 </html>
